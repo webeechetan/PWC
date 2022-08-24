bbf9754b6e08635f314453bf47b888fe298c2a19
Chetan Singh <chetan.singh@webeesocial.com>
2022-08-24 16:22:30 +0530

    added season system

---
+++ app/Models/StartupLogin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Scopes\SeasonScope;
+
+class StartupLogin extends Model
+{
+    use HasFactory;
+    protected $table = 'startup_login';
+    protected $fillable = [
+        'is_active',
+        'fullname',
+        'email',
+        'mobile',
+        'password',
+        'season_id'
+    ];
+
+    public function startup() {
+        return $this->belongsTo(Startup::class, 'startup_id', 'id');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new SeasonScope);
+    }
+}
